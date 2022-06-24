d5ecc5d15cc4dd2134471cc81c970c3f82c4c03b
a7madsam <lolahmad987@gmail.com>
2022-06-24 20:40:15 +0300

    pages added

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import { Route, Routes } from "react-router-dom";
+function App() {
+  return <Routes></Routes>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+// import App from "./final/App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <h1>React App</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
